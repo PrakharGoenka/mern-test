ea84edc7a6c88bfa9da76e7aee36468fc274d8f3
PrakharGoenka <prakhar.goinka@gmail.com>
2020-01-25 14:28:27 +0530

    Create React App.

---
+++ react-frontend/src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    state = {
+
+    }
+
+    componentDidMount() {
+        console.log('Its all workin')
+    }
+
+    render() {
+        return 'Hello World'
+    }
+}
+
+export default Homepage;
